f07b4954c4edd746b9e101d8da6280e5026ada42
oluwasayodev <oluwasayo.dev@gmail.com>
2023-05-01 23:18:31 +0100

    added sql output screenshots

--- queries.sql
+++ queries.sql
@@ -15,6 +15,8 @@ CREATE TABLE IF NOT EXISTS covid_19_data (
     recovered INT
 );
 
+-- Question 1
+-- Retrieve the total confirmed, death, and recovered cases.
 SELECT
     SUM(confirmed) as total_confirmed, 
     SUM(deaths) as total_deaths, 
@@ -22,6 +24,9 @@ SELECT
 FROM covid_19_data;
 
 
+-- Question 2
+-- Retrieve the total confirmed, deaths and recovered cases for the first quarter
+-- of each year of observation.
 SELECT
 	EXTRACT('year' FROM "observationDate") as year,
 	SUM(confirmed) as total_confirmed, 
@@ -32,6 +37,12 @@ WHERE EXTRACT('month' FROM "observationDate") BETWEEN 1 AND 3
 GROUP BY year;
 
 
+-- Question 3
+-- Retrieve a summary of all the records. This should include the following
+-- information for each country:
+-- ● The total number of confirmed cases
+-- ● The total number of deaths
+-- ● The total number of recoveries
 SELECT
 	country,
 	SUM(confirmed) as total_confirmed, 
@@ -41,7 +52,9 @@ FROM covid_19_data
 GROUP BY country
 ORDER BY country;
 
-
+-- Question 4
+-- Retrieve the percentage increase in the number of death cases from 2019 to
+-- 2020.
 SELECT round(((total_deaths_2020 - total_deaths_2019) / total_deaths_2019 ) * 100, 2) AS "percentage_increase(%)"
 FROM (
 	SELECT 
@@ -51,6 +64,8 @@ FROM (
 ) subq;
 
 
+-- Question 5
+-- Retrieve information for the top 5 countries with the highest confirmed cases.
 SELECT
 	country,
 	SUM(confirmed) as totalConfirmed
@@ -59,6 +74,10 @@ GROUP BY country
 ORDER BY totalConfirmed DESC
 LIMIT 5;
 
+
+-- Question 6
+-- Compute the total number of drop (decrease) or increase in the confirmed
+-- cases from month to month in the 2 years of observation.
 SELECT 
 	year,
     month, 
@@ -71,4 +90,4 @@ SELECT
     FROM covid_19_data
     GROUP BY year, month
     ORDER BY year, month
- ) subq;
+) subq;
